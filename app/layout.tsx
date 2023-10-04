30c70f5e9474253cf1e86f2da4a2d57f4fa1acb4
brunogbritto <brunoogbritto@gmail.com>
2023-10-04 00:39:23 -0300

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import "./globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Bruno Britto | Personal Porfolio",
+  description:
+    "Bruno is a full stack developer & p  ost graduated in digital law and compliance.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.className} bg-slate-100 text-gray-950 relative`}
+      >
+        <div className="bg-[#c0d8ff] absolute top-[-6rem] right-[11rem] h-[31.25rem] -z-10 w-[31.25rem] rounded-full blur-[10rem] opacity-70 sm:w-[68.75rem]"></div>
+        <div className="bg-[#afccfd] absolute top-[-1rem] left-[-35rem] h-[31.25rem] -z-10 w-[50rem] rounded-full blur-[10rem] opacity-70 sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+        {children}
+      </body>
+    </html>
+  );
+}
