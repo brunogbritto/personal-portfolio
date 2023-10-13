0c2fff2e3cae1567b2b95d5f48e67a53ed38b6d6
brunogbritto <brunoogbritto@gmail.com>
2023-10-13 17:20:27 -0300

    experience section added

---
+++ components/experience.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import React from "react";
+import SectionHeading from "./section-header";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import { experiencesData } from "@/lib/data";
+import { useSectionInView } from "@/lib/hooks";
+
+export default function Experience() {
+  const { ref } = useSectionInView("Experience");
+  return (
+    <section id="experience" ref={ref}>
+      <SectionHeading>Minha experiência</SectionHeading>
+      <VerticalTimeline>
+        {experiencesData.map((item, index) => (
+          <React.Fragment key={index}>
+            <VerticalTimelineElement visible={true}>
+              <h3>{item.title}</h3>
+              <p>{item.location}</p>
+              <p>{item.description}</p>
+            </VerticalTimelineElement>
+          </React.Fragment>
+        ))}
+      </VerticalTimeline>
+    </section>
+  );
+}
