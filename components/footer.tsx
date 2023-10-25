fa5634102b15131f52b1de2e8ee7c3e7e11220e1
brunogbritto <brunoogbritto@gmail.com>
2023-10-25 17:58:39 -0300

    footer added

---
+++ components/footer.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function footer() {
+  return (
+    <footer className="mb-10 px-4 text-center text-gray-500">
+      <small className="mb-2 block text-xs">
+        &copy; 2023 Bruno Grave de Britto. Todos os direitos reservados.
+      </small>
+      <p className="text-xs">
+        <span className="font-semibold">Sobre este site:</span> criado com React
+        & Next.js (App Router & Server Actions), Typescript, Tailwind CSS,
+        Framer Motion e React Resend.
+      </p>
+    </footer>
+  );
+}
