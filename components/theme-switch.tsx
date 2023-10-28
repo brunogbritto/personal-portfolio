830a61c5615354284c8ed17cb2dc67231f38d748
brunogbritto <brunoogbritto@gmail.com>
2023-10-27 22:57:26 -0300

    toggle theme feature added

--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -1,13 +1,38 @@
-import React from "react";
-import { BsSun } from "react-icons/bs";
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { BsMoon, BsSun } from "react-icons/bs";
+
+type Theme = "light" | "dark";
 
 export default function ThemeSwitch() {
+  const [theme, setTheme] = useState<Theme>("light");
+  const toggleTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
+      window.localStorage.setItem("theme", "dark");
+    } else {
+      setTheme("light");
+      window.localStorage.setItem("theme", "light");
+    }
+  };
+
+  useEffect(() => {
+    const localTheme = window.localStorage.getItem("theme") as Theme | null;
+    if (localTheme) {
+      setTheme(localTheme);
+    } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      setTheme("dark");
+    }
+  }, []);
+
   return (
     <button
-      className="fixed bottom-10 right-10 bg-white h-[3rem] w-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-black/20 border-opacity-40 shadow-2xl
-    rounded-full flex items-center justify-center"
+      className="fixed bottom-10 right-10 bg-white h-[3rem] w-[3rem] bg-opacity-80 backdrop-blur-[0.01rem] border border-black/20 border-opacity-40 shadow-2xl
+    rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all"
+      onClick={toggleTheme}
     >
-      <BsSun />
+      {theme === "light" ? <BsSun /> : <BsMoon />}
     </button>
   );
 }
