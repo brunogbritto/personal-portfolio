825899d22d040989961963b4505b086c6fa719b4
brunogbritto <brunoogbritto@gmail.com>
2023-10-30 23:27:48 -0300

    dark class added

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,8 @@ export default function RootLayout({
   return (
     <html lang="en" className="!scroll-smooth">
       <body
-        className={`${inter.className} bg-slate-100 text-gray-950 relative pt-28  sm:pt-36`}
+        className={`${inter.className} bg-slate-100 text-gray-950 relative pt-28  sm:pt-36
+        dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
       >
         <div className="bg-[#c0d8ff] absolute top-[-6rem] right-[11rem] h-[31.25rem] -z-10 w-[31.25rem] rounded-full blur-[10rem] opacity-70 sm:w-[68.75rem]"></div>
         <div className="bg-[#afccfd] absolute top-[-1rem] left-[-35rem] h-[31.25rem] -z-10 w-[50rem] rounded-full blur-[10rem] opacity-70 sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
