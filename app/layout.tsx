2b84bd265ed21591030f9adf09cf56ca088384e3
brunogbritto <brunoogbritto@gmail.com>
2023-11-10 20:13:18 -0300

    layout change

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,15 +26,43 @@ export default function RootLayout({
         className={`${inter.className} bg-slate-100 text-gray-950 relative pt-28  sm:pt-36
         dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
       >
+        {/* Primeira div original */}
         <div
           className="bg-[#c0d8ff] absolute top-[-6rem] right-[11rem] h-[31.25rem] -z-10 w-[31.25rem] rounded-full blur-[10rem] opacity-70 
-        sm:w-[68.75rem]
-        dark:bg-[#233884]"
+          sm:w-[68.75rem]
+          dark:bg-[#233884]"
         ></div>
+
+        {/* Segunda div original */}
         <div
           className="bg-[#afccfd] absolute top-[-1rem] left-[-35rem] h-[31.25rem] -z-10 w-[50rem] rounded-full blur-[10rem] opacity-70 
-        sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]
-        dark:bg-[#727faf]"
+          sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]
+          dark:bg-[#727faf]"
+        ></div>
+
+        {/* Terceira div adicionada */}
+        <div
+          className="bg-[#afccfd] absolute bottom-[160rem] right-[-30rem] h-[31.25rem] -z-10 w-[50rem] rounded-full blur-[10rem] opacity-70 
+          sm:w-[68.75rem] md:right-[-28rem] lg:right-[-22rem] xl:right-[-10rem] 2xl:right-[0rem]
+          dark:bg-[#727faf]"
+        ></div>
+        {/* Quarta div adicionada */}
+        <div
+          className="bg-[#afccfd] absolute bottom-[140rem] left-[-1rem] h-[31.25rem] -z-10 w-[50rem] rounded-full blur-[10rem] opacity-70 
+          sm:w-[68.75rem] md:right-[-28rem] lg:right-[-22rem] xl:right-[-10rem] 2xl:right-[0rem]
+          dark:bg-[#233884]"
+        ></div>
+        {/* Quinta div adicionada */}
+        <div
+          className="bg-[#afccfd] absolute bottom-[1rem] left-[-1rem] h-[31.25rem] -z-10 w-[50rem] rounded-full blur-[10rem] opacity-70 
+          sm:w-[68.75rem] md:right-[-28rem] lg:right-[-22rem] xl:right-[-10rem] 2xl:right-[0rem]
+          dark:bg-[#727faf]"
+        ></div>
+        {/* Sexta div adicionada */}
+        <div
+          className="bg-[#afccfd] absolute bottom-[1rem] right-[-30rem] h-[31.25rem] -z-10 w-[50rem] rounded-full blur-[10rem] opacity-70 
+          sm:w-[68.75rem] md:right-[-28rem] lg:right-[-22rem] xl:right-[-10rem] 2xl:right-[0rem]
+           dark:bg-[#233884]"
         ></div>
         <ThemeContextProvider>
           <ActiveSectionContextProvider>
