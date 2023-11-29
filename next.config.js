e14570bb5de4c3d01e43ab85b40be5d16f40af11
brunogbritto <brunoogbritto@gmail.com>
2023-11-29 10:02:21 -0300

    next config changed

--- next.config.js
+++ next.config.js
@@ -1,15 +1,7 @@
-const path = require("path");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     serverActions: true,
-  },
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    // Adiciona um alias para '@' apontando para a pasta 'src'
-    config.resolve.alias["@"] = path.join(__dirname, "src");
-
-    return config;
   },
 };
 
