fa79c49664a8225b85e3f12fb5b7e7bcc74d76c8
brunogbritto <brunoogbritto@gmail.com>
2023-12-01 19:24:23 -0300

    project changes

--- lib/data.ts
+++ lib/data.ts
@@ -98,20 +98,6 @@ export const projectsData = [
     tags: ["TypeScript", "React", "Next.js", "Tailwind", "MongoDB", "Express"],
     imageUrl: brecholaImg,
   },
-  {
-    title: "rmtDev",
-    description:
-      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-    imageUrl: rmtdevImg,
-  },
-  {
-    title: "Word Analytics",
-    description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-    imageUrl: wordanalyticsImg,
-  },
 ] as const;
 
 export const skillsData = [
